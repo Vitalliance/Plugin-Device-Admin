825c26c5dc3c1c0e0be8e0ba6c6649a7e88201da
Paul <paul.pauwels@vitalliance.fr>
2018-07-03 16:58:51 +0200

    New Changement v2

--- src/android/DeviceAdmin.java
+++ src/android/DeviceAdmin.java
@@ -7,15 +7,18 @@ import org.apache.cordova.CallbackContext;
 import android.R;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.provider.Settings.NameValueTable;
-import android.provider.Settings.Global;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.Intent;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.text.TextUtils;
+import android.view.accessibility.AccessibilityManager;
 
  
 public class DeviceAdmin extends CordovaPlugin {
  
  private Context mContext = null;
- @Override
+
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     if("Admin".equals(action)){
       callbackContext.success(this.DeviceAdminSample() ? 1 : 0);
