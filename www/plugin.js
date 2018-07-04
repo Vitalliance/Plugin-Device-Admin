dac7ca9eac741820cb3a8bcba8dbf310f323263c
Paul <paul.pauwels@vitalliance.fr>
2018-07-04 15:43:11 +0200

    New Changement v11

--- www/plugin.js
+++ www/plugin.js
@@ -3,11 +3,11 @@
 exports.GetDeviceAdmin = function(Success,error){
 	cordova.exec(function(DeviceAdmin){
 		//Success(DeviceAdmin);
+		Success();
 		alert("titi");
 	},error,"DeviceAdmin","Admin",[]);
 };
 
-alert("toto");
 
 // Plugin.Admin = function(){
 //   var onSuccess = function(data){
