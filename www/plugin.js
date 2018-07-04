480d797efe03c7b2e6bfc38dfbfefa5ddf06d2d0
Paul <paul.pauwels@vitalliance.fr>
2018-07-04 15:57:29 +0200

    New Changement v12

--- www/plugin.js
+++ www/plugin.js
@@ -1,21 +1,19 @@
 //var exec = require('cordova/exec');
 
-exports.GetDeviceAdmin = function(Success,error){
-	cordova.exec(function(DeviceAdmin){
-		//Success(DeviceAdmin);
-		Success();
-		alert("titi");
-	},error,"DeviceAdmin","Admin",[]);
-};
-
-
-// Plugin.Admin = function(){
-//   var onSuccess = function(data){
-//   	console.log(data);
-//   	return data;
-//   };
-//   var onFail = function(){};
-//   cordova.exec(onSuccess, onFail, 'DeviceAdmin', 'Admin',[]);
+// exports.GetDeviceAdmin = function(Success,error){
+// 	cordova.exec(function(DeviceAdmin){
+// 		//Success(DeviceAdmin);
+// 		Success();
+// 		alert("titi");
+// 	},error,"DeviceAdmin","Admin",[]);
 // };
-// module.exports = DeviceAdmin;
+
+
+Plugin.GetDeviceAdmin = function(){
+  var onSuccess = function(){
+  	alert("titi");
+  };
+  var onFail = function(){};
+  cordova.exec(onSuccess, onFail, 'AlertPlugin', 'alert', []);
+};
 
