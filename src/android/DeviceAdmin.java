9e26ebc6d20f8168409de4e2fa37a699f84a691b
Paul <paul.pauwels@vitalliance.fr>
2018-07-04 18:21:37 +0200

    New Changement v15

--- src/android/DeviceAdmin.java
+++ src/android/DeviceAdmin.java
@@ -1,5 +1,5 @@
 package com.example.cordova.DeviceAdmin;
- 
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.apache.cordova.CordovaPlugin;
@@ -10,35 +10,37 @@ import android.content.DialogInterface;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.content.Intent;
 import android.provider.Settings;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.apache.cordova.PluginResult;
 import android.provider.Settings.SettingNotFoundException;
 import android.text.TextUtils;
 import android.view.accessibility.AccessibilityManager;
 
- 
+
 public class DeviceAdmin extends CordovaPlugin {
- 
+
  private Context mContext = null;
 
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if("Admin".equals(action)){
-      callbackContext.success();
-      return true;
-    } else {
-      callbackContext.error("DeviceAdminSample ."+action+" not found !");
-      return false;
-    }
+ public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+  if ("Admin".equals(action)) {
+
+   int adb = Settings.Secure.getInt(mContext.getContentResolver(),
+    Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0);
+
+   JSONObject json = new JSONObject().put("DEVELOPMENT_SETTINGS_ENABLED", adb);
+
+   PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, json);
+   pluginResult.setKeepCallback(true);
+   pushContext.sendPluginResult(pluginResult);
+
+   return true;
+  } else {
+   callbackContext.error("DeviceAdminSample ." + action + " not found !");
+   return false;
   }
+ }
 
- 
-  public boolean DeviceAdminSample(){
 
-    int adb = Settings.Secure.getInt(mContext.getContentResolver(),
-              Settings.Global.DEVELOPMENT_SETTINGS_ENABLED , 0);
-
-    if (adb != 0) 
-      return true;
-    else
-      return false;
-
-  }
-}   
+}
--- www/plugin.js
+++ www/plugin.js
@@ -1,4 +1,4 @@
-//var exec = require('cordova/exec');
+var exec = cordova.require('cordova/exec');
 
 // exports.GetDeviceAdmin = function(Success,error){
 // 	cordova.exec(function(DeviceAdmin){
@@ -9,12 +9,27 @@
 // };
 
 
-Plugin.GetDeviceAdmin = function(){
-  var onSuccess = function(){
-  	alert("titi");
-  };
-  var onFail = function(){};
-  cordova.exec(onSuccess, onFail, 'DeviceAdmin', 'Admin', []);
-};
+// Plugin.GetDeviceAdmin = function(){
+//   var onSuccess = function(){
+//   	alert("titi");
+//   };
+//   var onFail = function(){};
+//   cordova.exec(onSuccess, onFail, 'DeviceAdmin', 'Admin', []);
+// };
 
-module.exports = Plugin;
+// module.exports = Plugin;
+
+var GetDeviceAdmin = function() {
+
+    setTimeout(function() {
+        exec(function(result) {
+                successCallback(result);
+            },
+            function(error) {
+                errorCallback(error);
+            }, 'DeviceAdmin', 'Admin', []);
+    }, 10);
+
+}
+
+module.exports = GetDeviceAdmin;
