9bac5a39c96df83f68d1055be96c1ecee3d939a7
Paul <paul.pauwels@vitalliance.fr>
2018-07-09 10:58:18 +0200

    New Changement v34

--- src/android/DeviceAdmin.java
+++ src/android/DeviceAdmin.java
@@ -29,11 +29,11 @@ public class DeviceAdmin extends CordovaPlugin {
    int adb = Settings.Secure.getInt(mContext.getContentResolver(),
     Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0);
 
-   JSONObject json = new JSONObject().put("DEVELOPMENT_SETTINGS_ENABLED", adb);
+   // JSONObject json = new JSONObject().put("DEVELOPMENT_SETTINGS_ENABLED", adb);
 
-   PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, json);
-   pluginResult.setKeepCallback(true);
-   callbackContext.sendPluginResult(pluginResult);
+   // PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, json);
+   // pluginResult.setKeepCallback(true);
+   callbackContext.success(adb);
 
    return true;
   } else {
